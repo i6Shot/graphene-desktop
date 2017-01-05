db92f09c50face710b39437075a39a7cc8f2c4dd
Aidan Shafran <zelbrium@gmail.com>
2017-01-05 13:12:45 -0500

    Correct layout for panel clock; panel shadow

--- src/panel.c
+++ src/panel.c
@@ -8,7 +8,8 @@
 #include "cmk/shadow.h"
 #include "applets/clock.h"
 
-#define PANEL_HEIGHT 32 // Pixels; multiplied by the window scale factor
+#define PANEL_HEIGHT 64 // Pixels; multiplied by the window scale factor
+#define SHADOW_HEIGHT 20
 
 struct _GraphenePanel
 {
@@ -17,7 +18,9 @@ struct _GraphenePanel
 	CPanelModalCallback modalCb;
 	gpointer cbUserdata;
 
+	CmkShadowContainer *sdc;
 	CmkWidget *bar;
+	CmkWidget *clock;
 	CmkWidget *popup;
 };
 
@@ -57,10 +60,24 @@ static void graphene_panel_init(GraphenePanel *self)
 	clutter_actor_set_reactive(CLUTTER_ACTOR(self->bar), TRUE);
 	cmk_widget_set_draw_background_color(self->bar, TRUE);
 	cmk_widget_set_background_color(self->bar, "background");
+
+	clutter_actor_set_layout_manager(self->bar, clutter_box_layout_new());
+
+	self->sdc = cmk_shadow_container_new();
+	cmk_shadow_container_set_vblur(self->sdc, SHADOW_HEIGHT);
+	clutter_actor_add_child(CLUTTER_ACTOR(self), CLUTTER_ACTOR(self->sdc));
 	clutter_actor_add_child(CLUTTER_ACTOR(self), CLUTTER_ACTOR(self->bar));
 
+	// Sample launcher + tasklist
+	ClutterActor *launcher = clutter_actor_new();
+	clutter_actor_add_child(self->bar, launcher);
+
+	ClutterActor *tasklist = clutter_actor_new();
+	clutter_actor_set_x_expand(tasklist, TRUE);
+	clutter_actor_add_child(self->bar, tasklist);
+
 	GrapheneClockApplet *clock = graphene_clock_applet_new();
-	cmk_widget_set_style_parent(clock, self->bar);
+	self->clock = clock;
 	clutter_actor_add_child(self->bar, clock);
 }	
 
@@ -69,6 +86,9 @@ static void graphene_panel_allocate(ClutterActor *self_, const ClutterActorBox *
 	GraphenePanel *self = GRAPHENE_PANEL(self_);
 	
 	ClutterActorBox barBox = {box->x1, box->y2-PANEL_HEIGHT, box->x2, box->y2};
+	ClutterActorBox sdcBox = {box->x1, box->y2-PANEL_HEIGHT-SHADOW_HEIGHT, box->x2, box->y2};
+
+	clutter_actor_allocate(CLUTTER_ACTOR(self->sdc), &sdcBox, flags);
 	clutter_actor_allocate(CLUTTER_ACTOR(self->bar), &barBox, flags);
 
 	CLUTTER_ACTOR_CLASS(graphene_panel_parent_class)->allocate(self_, box, flags);
@@ -77,6 +97,11 @@ static void graphene_panel_allocate(ClutterActor *self_, const ClutterActorBox *
 static void on_style_changed(CmkWidget *self_, CmkStyle *style)
 {
 	GraphenePanel *panel = GRAPHENE_PANEL(self_);
+
+	float padding = cmk_style_get_padding(style);
+	ClutterMargin margin = {padding, padding, 0, 0};
+	clutter_actor_set_margin(GRAPHENE_PANEL(self_)->clock, &margin);
+
 	CMK_WIDGET_CLASS(graphene_panel_parent_class)->style_changed(self_, style);
 }
 
